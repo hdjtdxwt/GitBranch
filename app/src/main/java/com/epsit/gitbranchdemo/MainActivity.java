8d27921ef50eb399a965ac8ae39343da0930a072
wangtao <wangtao@epsit.cn>
2019-05-21 17:58:46 +0800

    功能1完成，提交测试

--- app/src/main/java/com/epsit/gitbranchdemo/MainActivity.java
+++ app/src/main/java/com/epsit/gitbranchdemo/MainActivity.java
@@ -2,12 +2,24 @@ package com.epsit.gitbranchdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btn1).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn1:
+                Toast.makeText(this, "btn1点击了", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
