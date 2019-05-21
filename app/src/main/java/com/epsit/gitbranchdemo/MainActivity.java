5a7f35c82b009208e895c3bfe3958903a54bbc6a
wangtao <wangtao@epsit.cn>
2019-05-21 18:42:08 +0800

    罗湖分支发现bug，紧急修复

--- app/src/main/java/com/epsit/gitbranchdemo/MainActivity.java
+++ app/src/main/java/com/epsit/gitbranchdemo/MainActivity.java
@@ -3,18 +3,34 @@ package com.epsit.gitbranchdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
+    Button btn1,btn2,btn3,btn4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.btn1).setOnClickListener(this);
-        findViewById(R.id.btn2).setOnClickListener(this);
-        findViewById(R.id.btn3).setOnClickListener(this);
-        findViewById(R.id.btn4).setOnClickListener(this);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+
+        if(btn1!=null){
+            btn1.setOnClickListener(this);
+        }
+
+        if(btn2!=null){
+            btn2.setOnClickListener(this);
+        }
+        if(btn3!=null){
+            btn3.setOnClickListener(this);
+        }
+        if(btn4!=null){
+            btn4.setOnClickListener(this);
+        }
+
     }
 
     @Override
