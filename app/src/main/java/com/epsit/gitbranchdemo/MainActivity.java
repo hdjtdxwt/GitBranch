ef6b85748aacdf46a82727e4190ed2ce933ebe44
wangtao <hdjtdxwt@126.com>
2020-10-30 09:45:24 +0800

    1.修改会原来的样子

--- app/src/main/java/com/epsit/gitbranchdemo/MainActivity.java
+++ app/src/main/java/com/epsit/gitbranchdemo/MainActivity.java
@@ -13,9 +13,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViewById(R.id.btn1).setOnClickListener(this);
-
-        findViewById(R.id.btn2).setOnClickListener(this);
-
     }
 
     @Override
@@ -24,12 +21,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn1:
                 Toast.makeText(this, "测试", Toast.LENGTH_SHORT).show();
                 break;
-
-            case R.id.btn2: {
-                Toast.makeText(this, "测试2", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(this, TestActivity.class));
-            } break;
-
         }
     }
 }
